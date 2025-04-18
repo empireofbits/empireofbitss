b4a66c4d25f38e27bd199781ee354631a0527724
kunalcode12 <kunal818196@gmail.com>
2025-04-19 00:44:13 +0530

    first commit

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+// next.config.ts
+import type { Configuration } from 'webpack';
+
+const nextConfig = {
+  webpack: (config: Configuration, { isServer }: { isServer: boolean }) => {
+    if (!isServer) {
+      config.resolve = config.resolve || {};
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
